13f472d9499e38896b6d4c0ce016ecf6c78c442a
AminTabrizi <mohanmad1386tabrizi4@gmail.com>
2026-02-23 13:12:14 +0330

    feat: add post templates and urls

---
+++ core/blog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
+app_name = "blog"
+
+urlpatterns = [
+    # path("fbx-index", views.indexView,name='fbv-index'),
+    # path("cbv-index", TemplateView.as_view(template_name="index.html", extra_context={"name":"Amin"})),
+    # path("go-to-django/<int:pk>/", views.RedirectToDjango.as_view(),name="redirect-to-index",),
+    # path("cbv-index/", views.IndexView.as_view(), name='cbv-index'),
+    path("post/", views.PostListView.as_view(), name='post-list'),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/create/", views.PostCreateView.as_view(), name="post-create"),
+
+]
