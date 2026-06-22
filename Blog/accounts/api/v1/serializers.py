f253715871c35fae0a467b50a7d858b8a9f26a0d
AminTabrizi <mohanmad1386tabrizi4@gmail.com>
2026-06-22 18:18:40 +0330

    feat(profile): add profile retrieve and update API

--- Blog/accounts/api/v1/serializers.py
+++ Blog/accounts/api/v1/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ...models import User
+from ...models import User, Profile
 from django.contrib.auth.password_validation import validate_password
 from django.core import exceptions
 from django.contrib.auth import authenticate
@@ -91,3 +91,11 @@ class ChangePasswordSerializer(serializers.Serializer):
             
 
         return super().validate(attrs)
+    
+class ProfileSerializer(serializers.ModelSerializer):
+    email = serializers.CharField(source='user.email', read_only = True)
+
+    class Meta:
+        model = Profile
+        fields = ('id','email', 'first_name', 'last_name', 'image', 'description',)
+        read_only_fields = ('email',)
--- Blog/accounts/api/v1/views.py
+++ Blog/accounts/api/v1/views.py
@@ -7,8 +7,9 @@ from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from django.contrib.auth import get_user_model
+from ...models import Profile
+from django.shortcuts import get_object_or_404
 
 user = get_user_model
 
@@ -71,4 +72,14 @@ class ChangePasswordApiView(generics.GenericAPIView):
             self.object.save()
             return Response({'details':'password changed successfully'}, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class ProfileApiView(generics.RetrieveUpdateAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+
+    def get_object(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, user=self.request.user)
+        return obj
+    
     
